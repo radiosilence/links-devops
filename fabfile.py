20f45ef6c583dd2275b41d0069d97b7c15c76b9e
James Cleveland <jc@blackflags.co.uk>
2013-03-05 14:35:55 +0000

    DB Stuff

--- fabfile.py
+++ fabfile.py
@@ -14,10 +14,14 @@ env.hosts = ['linkscreative.co.uk:22734']
 env.repo = os.environ.get('REPO', None) or 'devops'
 env.project = os.environ.get('PROJECT', None) or 'links-creative'
 env.app = os.environ.get('APP', None) or env.repo
+env.db_adapter = 'mysql'
+env.db_user = os.environ.get('DB_USER')
+env.db_password = os.environ.get('DB_PASSWORD')
+
 
 
 def _random(length=16):
-    return ''.join([random.choice(string.digits + string.letters + u'!£$^)( %+-_:;.,~')
+    return ''.join([random.choice(string.digits + string.letters + u'!-_:;.,^&')
                     for i
                     in range(0, length)])
 
@@ -56,12 +60,31 @@ def install_requirements():
     run('pip install -r requirements.txt')
 
 
+def setup_database_mysql():
+    user = "'{env.repo}_{env.instance}'@'localhost'".format(env=env)
+    db = '{env.repo}_{env.instance}'.format(env=env)
+    run('echo "{commands}" | mysql -u {env.db_user} -p{env.db_password} '.format(env=env,
+        commands=''.join([
+            'CREATE DATABASE IF NOT EXISTS {db};'.format(db=db),
+            "CREATE USER {user} IDENTIFIED BY '{password}';".format(
+                user=user, password=env.secrets['db']),
+            'GRANT ALL on {db}.* TO {user};'.format(db=db, user=user),
+        ])
+    ))
+
+def setup_database():
+    if env.db_adapter == 'postgres':
+        setup_database_postgres()
+    elif env.db_adapter == 'mysql':
+        setup_database_mysql()
+
 def create_instance(instance):
     _init(instance)
     env.secrets = {
         'db': _random(),
         'key': _random(64),
     }
+    setup_database()
     run(u'mkdir -p {env.virtualenv}'.format(env=env))
     run(u'virtualenv {env.virtualenv}'.format(env=env))
     run(u'mkdir -p {}'.format(env.directory))
@@ -72,9 +95,14 @@ def create_instance(instance):
 
     with virtualenv():
         run('pip install -r requirements.txt')
+    
+    with virtualenv():
+        run('python manage.py syncdb --noinput')
+        run('python manage.py migrate --noinput')
+        run('python manage.py collectstatic --noinput')
 
-    print 'Database password: {}'.format(env.secrets['db'])
-    print 'Secret key: {}'.format(env.secrets['key'])
+    print 'Database password:  {}'.format(env.secrets['db'])
+    print 'Secret key:         {}'.format(env.secrets['key'])
     
 
 def upgrade(instance):
