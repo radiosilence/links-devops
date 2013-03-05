c6a54627d0ae84ff9e2bf32b6c0344d6431c869e
James Cleveland <jc@blackflags.co.uk>
2013-03-05 14:52:15 +0000

    Noms

--- fabfile.py
+++ fabfile.py
@@ -37,46 +37,46 @@ def _init(instance):
     env.virtualenv = u'/env/{env.repo}/{env.instance}'.format(env=env)
     env.activate = u'source {env.virtualenv}/bin/activate'.format(env=env)
 
-def generate_post_activate():
+
+def generate_vars():
     variables = {
-        'DJANGO_SETTINGS_MODULE': u'{env.app}.settings.production}'.format(env=env),
+        'DJANGO_SETTINGS_MODULE': u'{env.app}.settings.production'.format(env=env),
         'DJANGO_DB_PASSWORD': env.secrets['db'],
-        'DJANGO_SETTINGS_MODULE': env.secrets['key'],
+        'DJANGO_SECRET_KEY': env.secrets['key'],
     }
     if env.instance == 'test':
         variables['DJANGO_SETTINGS_MODULE'] = u'{env.app}.settings.local'.format(env=env)
-
-
-
-def pull():
-    local("git pull --rebase")
-
-
-def push():
-    local("git push")
+    return variables
+    
 
 
 def install_requirements():
     run('pip install -r requirements.txt')
 
 
+def run_mysql(command):
+    run('echo "{command}" | mysql -u {env.db_user} -p{env.db_password} '.format(env=env,
+        command=command
+    ))
+
+
 def setup_database_mysql():
     user = "'{env.repo}_{env.instance}'@'localhost'".format(env=env)
     db = '{env.repo}_{env.instance}'.format(env=env)
-    run('echo "{commands}" | mysql -u {env.db_user} -p{env.db_password} '.format(env=env,
-        commands=''.join([
-            'CREATE DATABASE IF NOT EXISTS {db};'.format(db=db),
-            "CREATE USER {user} IDENTIFIED BY '{password}';".format(
-                user=user, password=env.secrets['db']),
-            'GRANT ALL on {db}.* TO {user};'.format(db=db, user=user),
-        ])
-    ))
+    with settings(warn_only=True):
+        run_mysql('CREATE DATABASE IF NOT EXISTS {db};'.format(db=db))
+        run_mysql('DROP USER {user}'.format(user=user))
+        run_mysql("CREATE USER {user} IDENTIFIED BY '{password}';".format(
+            user=user, password=env.secrets['db']))
+        run_mysql('GRANT ALL on {db}.* TO {user};'.format(db=db, user=user))
+
 
 def setup_database():
     if env.db_adapter == 'postgres':
         setup_database_postgres()
     elif env.db_adapter == 'mysql':
         setup_database_mysql()
+
 
 def create_instance(instance):
     _init(instance)
@@ -89,28 +89,26 @@ def create_instance(instance):
     run(u'virtualenv {env.virtualenv}'.format(env=env))
     run(u'mkdir -p {}'.format(env.directory))
     with cd(env.directory):
-        run(u'git clone git@codebasehq.com:linkscreative/{env.project}/{env.repo}.git .'.format(
-            env=env
-        ))
+        with settings(warn_only=True):
+            result = run(u'git clone git@codebasehq.com:linkscreative/{env.project}/{env.repo}.git .'.format(
+                env=env
+            ))
+            if result.failed:
+                run('git pull --rebase')
 
     with virtualenv():
         run('pip install -r requirements.txt')
-    
-    with virtualenv():
-        run('python manage.py syncdb --noinput')
-        run('python manage.py migrate --noinput')
-        run('python manage.py collectstatic --noinput')
 
-    print 'Database password:  {}'.format(env.secrets['db'])
-    print 'Secret key:         {}'.format(env.secrets['key'])
+    for k, v in generate_vars().items():
+        print '{0}: "{1}"'.format(k, v.replace('"', '\"'))
     
 
 def upgrade(instance):
     _init(instance)
     print(u'Updating {instance}'.format(instance=instance)) 
 
-    pull()
-    push()
+    local("git pull --rebase")
+    local("git push")
 
     remote_path = _remote_path(instance)
     with cd(remote_path):
