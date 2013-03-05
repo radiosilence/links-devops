57664931f8cdf841943eab7849d086c0197021fa
James Cleveland <jc@blackflags.co.uk>
2013-03-05 15:10:36 +0000

    A setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+
+requires = []
+dep_links = []
+
+for dep in open('requirements.txt').read().split("\n"):
+    if dep.startswith('git+'):
+        dep_links.append(dep)
+    else:
+        requires.append(dep)
+
+
+setup(
+    name="Links Creative DevOps",
+    version="0.1",
+    description="Fabric deployment script.",
+    author=u"James Cleveland",
+    author_email="james@dapperdogstudios.com",
+    url="https://linkscreative.codebasehq.com/projects/links-creative/repositories/devops",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    dependency_links=dep_links,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        ],
+    zip_safe=False,
+)
