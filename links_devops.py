54efc2c2b9a9ec2ae76f425bce54e2581be0c08b
James Cleveland <jc@blackflags.co.uk>
2013-06-18 14:24:26 +0100

    Do not set user by default

--- links_devops.py
+++ links_devops.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 from devops import *
-env.user = 'wsgi'
 env.hosts = ['linkscreative.co.uk:22734']
 env.db_adapter = 'mysql'
 env.virtualenv_template = u'/env/{env.repo}/{env.instance}'
